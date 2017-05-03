b5201ae437b92168847164b8b1f5408f6ae85231
Ivan Soldatov <yazvyazda@mail.ru>
2017-05-03 15:46:53 +0300

    add elem

--- common.blocks/popup/popup.js
+++ common.blocks/popup/popup.js
@@ -1,10 +1,10 @@
-modules.define('popup', ['i-bem-dom', 'popup__close'], function(provide, bemDom, Close, Popup) {
+modules.define('popup', ['i-bem-dom', 'popup__close', 'button'], function(provide, bemDom, Close, Button, Popup) {
 
 provide(bemDom.declBlock(Popup, {
     onSetMod: {
         'js': {
             'inited': function() {
-                this._event(Close).on('click', function () {
+                this._events(this.findChildElem(Close)).on('click', function () {
                     this.delMod('visible');
                 });
             }
