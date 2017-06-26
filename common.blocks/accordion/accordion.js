71fcf6eca87f92504bef922dd90a370ea7505d6d
Ivan Soldatov <yazvyazda@mail.ru>
2017-06-26 14:03:08 +0300

    add js and css

--- common.blocks/accordion/accordion.js
+++ common.blocks/accordion/accordion.js
@@ -1,38 +1,35 @@
-modules.define('tabs', ['i-bem-dom', 'radio-group'], function(provide, bemDom, RadioGroup) {
+modules.define('accordion', ['i-bem-dom', 'checkbox'], function(provide, bemDom, Checkbox) {
 
 provide(bemDom.declBlock(this.name, {
     onSetMod : {
         'js' : {
             'inited' : function() {
-                var _this = this;
+                var _this = this,
+                    titles = this.findChildElems('title');
 
-                this._radiogroup = this.findChildBlock(RadioGroup);
-                this._listContent = [];
+                this._items = [];
 
-                this.findChildElems('content').forEach(function (item) {
-                    _this._listContent[item.params.id] = item;
+                titles._entities.forEach(function (item, i) {
+                    var titleName = item.findMixedBlock(Checkbox).getName();
+
+                    _this._items[titleName] = item.findParentElem('item');
                 });
-
                 // выполняется при первом BEM-событии "change"
             }
         }
     },
 
-    _onChangeRadioGroup: function(e) {
-        var radioGroupVal = this._radiogroup.getVal();
+    _onCheckboxChanged: function(e) {
+        var flag = e.bemTarget.hasMod('checked');
 
-        this.findChildElems('content').delMod('checked');
-        this._listContent[radioGroupVal].setMod('checked');
-
+        this._items[e.bemTarget.getName()].setMod('checked', flag);
         // выполняется при каждом DOM-событии "change"
     }
 }, {
     lazyInit : true,
 
     onInit : function() {
-        this._events(RadioGroup).on(
-            'change',
-            this.prototype._onChangeRadioGroup);  // в момент клика будет создан экземпляр блока и вызван его метод _onChangeRadioGroup
+        this._events(Checkbox).on({ modName: 'checked', modVal: '*' }, this.prototype._onCheckboxChanged);
     }
 }));
 
